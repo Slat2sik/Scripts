47c806bd79ffd3e81c4f035efb9fae3bdf732715
ThatGuy-oops <feedpro181@gmail.com>
2023-01-02 07:28:55 -0500

    Update Every Second You Get +1 Health.lua

--- Every Second You Get +1 Health.lua
+++ Every Second You Get +1 Health.lua
@@ -1,33 +1,98 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Made By J.#5424 | Every Second You Get +1 Health", "BloodTheme")
- 
--- Player
-local Player = Window:NewTab("Player")
-local PlayerSection = Player:NewSection("Player")
- 
-PlayerSection:NewButton("Admin Commands", "Infinite Yield", function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-end)
- 
-PlayerSection:NewSlider("Walk Speed", "Go Fast", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
-end)
- 
- 
-PlayerSection:NewSlider("Jump Power", "Go High", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
-    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
-end)
--- AUTO FARM
-local Farm = Window:NewTab("Auto Farm")
-local FarmSection = Farm:NewSection("Auto Farm")
- 
- 
-FarmSection:NewButton("Infinite Wins (+12 Wins / 9 Seconds)", "+12 Wins every 9 seconds (rejoin to stop)", function()
-    while true do
-        local x = 984.4512329101562
-        local y = 0.20754379034042358
-        local z = 329.2272644042969
-        game.Players.LocalPlayer.Character:MoveTo(Vector3.new(x, y, z))
-            wait(9)
-        end
-end)
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Player = game.Players.LocalPlayer
+local Window = OrionLib:MakeWindow({Name = "Made By J.#5424", HidePremium = false, SaveConfig = true, IntroEnabled = false})
+
+-- Noti
+
+OrionLib:MakeNotification({
+	Name = "Enjoy the script",
+	Content = "You are logged in as "..Player.Name.." ",
+	Image = "rbxassetid://4483345998",
+	Time = 7
+})
+
+
+-- Functions
+
+function EnabledNotification()
+    OrionLib:MakeNotification({
+        Name = "Enabled!",
+        Content = "Auto farm has been enabled",
+        Image = "rbxassetid://4483345998",
+        Time = 5
+    })
+end
+
+function RejoinNotification()
+    OrionLib:MakeNotification({
+        Name = "Rejoining",
+        Content = "Rejoining the same server",
+        Image = "rbxassetid://4483345998",
+        Time = 5
+    })
+end
+
+function DiscordServerNotification()
+    OrionLib:MakeNotification({
+        Name = "Copied!",
+        Content = "Successfully copied invite to clipboard",
+        Image = "rbxassetid://4483345998",
+        Time = 5
+    })
+end
+
+
+
+
+-- On the form
+
+local Tab = Window:MakeTab({
+	Name = "Auto Farm",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "Auto Farm Wins (click the rejoin button to stop)",
+	Callback = function()
+      		EnabledNotification()
+                while true do
+                    local x = 984.4512329101562
+                    local y = 0.20754379034042358
+                    local z = 329.2272644042969
+                    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(x, y, z))
+                        wait(9)
+                    end
+  	end    
+})
+
+Tab:AddButton({
+	Name = "Rejoin",
+	Callback = function()
+        RejoinNotification()
+        wait(2)
+        local ts = game:GetService("TeleportService")
+
+        local p = game:GetService("Players").LocalPlayer
+        
+         
+        
+        ts:Teleport(game.PlaceId, p)
+  	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = "Credits",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "Join discord server",
+	Callback = function()
+      		DiscordServerNotification()
+
+              setclipboard("https://discord.gg/BUA8XuPWGX")
+              toclipboard("https://discord.gg/BUA8XuPWGX")
+  	end    
+})
