2dcd55b9cb58ffe4ca405475c029fa43d815247a
ThatGuy-oops <feedpro181@gmail.com>
2023-01-09 17:34:30 -0500

    Changed the UI

--- EverySecondYouGet+1Health.lua
+++ EverySecondYouGet+1Health.lua
@@ -1,205 +1,193 @@
 if game.PlaceId == 11302865934 then
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Player = game.Players.LocalPlayer
-local Window = OrionLib:MakeWindow({Name = "Made By J.#5424", HidePremium = false, SaveConfig = true, IntroEnabled = false})
-
--- Noti
-
-OrionLib:MakeNotification({
-	Name = "Enjoy the script",
-	Content = "You are logged in as "..Player.Name.." ",
-	Image = "rbxassetid://4483345998",
-	Time = 7
-})
-
--- Values
-_G.autoWins = true
-_G.autoRebirth = true
-_G.autoHatch = true
-_G.selectEgg = "Noob"
-
-
--- Functions
-
-function autoWins()
-    while _G.autoWins == true do
-		local x = 2089.228271484375
-		local y = 205.49998474121094
-		local z = 145.49891662597656
-            game.Players.LocalPlayer.Character:MoveTo(Vector3.new(x, y, z))
-                wait(9)
-                wait('.0000000000000000000000000000000000000000000000000000000000000000001')
-            end
-end
-
-function autoRebirth()
-    while _G.autoRebirth == true do
-    game:GetService("ReplicatedStorage").RebirthEvent:FireServer()
-    wait('.0000000000000000000000000000000000000000000000000000000000000000001')
-    end
-end
-
-function autoHatch()
-	while _G.autoHatch == true do
-	game:GetService("ReplicatedStorage").RemoteEvents.EggOpened:InvokeServer(_G.selectEgg,"Single")
-	wait('.0000000000000000000000000000000000000000000000000000000000000000001')
+	local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+	local Window = Rayfield:CreateWindow({
+	   Name = "+1 Health",
+	   LoadingTitle = "Made By J.#5424",
+	   LoadingSubtitle = "by J.#5424",
+	--    ConfigurationSaving = {
+	--       Enabled = false,
+	--       FolderName = nil, -- Create a custom folder for your hub/game
+	--       FileName = "Big Hub"
+	--    },
+	   Discord = {
+		  Enabled = true,
+		  Invite = "https://discord.gg/BUA8XuPWGX", -- The Discord invite code, do not include discord.gg/
+		  RememberJoins = false -- Set this to false to make them join the discord every time they load it up
+	   },
+	   KeySystem = true, -- Set this to true to use our key system
+	   KeySettings = {
+		  Title = "Key System",
+		  Subtitle = "By J.#5424",
+		  Note = "F9 to get the key",
+		  FileName = "JKey",
+		  SaveKey = false,
+		  GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+		  Key = "JScripts",
+		--   print("Key: JScripts")
+	   }
+	})
+	
+	-- Values
+	_G.autoWins = true
+	_G.autoRebirth = true
+	_G.autoHatch = true
+	_G.selectEgg = "Noob"
+	
+	
+	-- Functions
+	
+	function autoWins()
+		while _G.autoWins == true do
+			local x = 2089.228271484375
+			local y = 205.49998474121094
+			local z = 145.49891662597656
+				game.Players.LocalPlayer.Character:MoveTo(Vector3.new(x, y, z))
+					wait(9)
+					wait('.0000000000000000000000000000000000000000000000000000000000000000001')
+				end
 	end
-end
-
-function DiscordServerNotification()
-    OrionLib:MakeNotification({
-        Name = "Copied!",
-        Content = "Successfully copied invite to clipboard",
-        Image = "rbxassetid://4483345998",
-        Time = 5
-    })
-end
-
-
-
-
--- On the form
-
-local Tab = Window:MakeTab({
-	Name = "Auto Farm",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
-Tab:AddToggle({
-	Name = "Auto Wins",
-	Default = false,
-	Callback = function(Value)
-            _G.autoWins = Value
-
-            autoWins()
-  	end    
-})
-
-Tab:AddToggle({
-	Name = "Auto Rebirth",
-	Default = false,
-	Callback = function(Value)
-		_G.autoRebirth = Value
-
-        autoRebirth()
-	end    
-})
-
-local Tab = Window:MakeTab({
-	Name = "Eggs",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
-Tab:AddParagraph("NOTE","To collect the eggs you must be standing inside the egg box")
-
-Tab:AddToggle({
-	Name = "Auto Hatch",
-	Default = false,
-	Callback = function(Value)
+	
+	function autoRebirth()
+		while _G.autoRebirth == true do
+		game:GetService("ReplicatedStorage").RebirthEvent:FireServer()
+		wait('.0000000000000000000000000000000000000000000000000000000000000000001')
+		end
+	end
+	
+	function autoHatch()
+		while _G.autoHatch == true do
+		game:GetService("ReplicatedStorage").RemoteEvents.EggOpened:InvokeServer(_G.selectEgg,"Single")
+		wait('.0000000000000000000000000000000000000000000000000000000000000000001')
+		end
+	end
+	
+	function DiscordServerNotification()
+		OrionLib:MakeNotification({
+			Name = "Copied!",
+			Content = "Successfully copied invite to clipboard",
+			Image = "rbxassetid://4483345998",
+			Time = 5
+		})
+	end
+	
+	-- On the form
+	
+	-- Auto Farm
+	
+	local Tab = Window:CreateTab("Auto Farm", 4483362458) -- Title, Image
+	
+	local Toggle = Tab:CreateToggle({
+	   Name = "Auto Wins",
+	   CurrentValue = false,
+	   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	   Callback = function(Value)
+			_G.autoWins = Value
+	
+			autoWins()
+	   end,
+	})
+	
+	local Toggle = Tab:CreateToggle({
+	   Name = "Auto Rebirths",
+	   CurrentValue = false,
+	   Flag = "Toggle2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	   Callback = function(Value)
+			_G.autoRebirth = Value
+	
+			autoRebirth()
+	   end,
+	})
+	
+	-- Eggs
+	
+	local Tab = Window:CreateTab("Eggs", 4483362458) -- Title, Image
+	
+	local Toggle = Tab:CreateToggle({
+	   Name = "Auto Hatch",
+	   CurrentValue = false,
+	   Flag = "Toggle3", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	   Callback = function(Value)
 		_G.autoHatch = Value
 		autoHatch()
-	end    
-})
-
-Tab:AddDropdown({
-	Name = "Select Egg",
-	Default = "Noob",
-	Options = {"Noob", "Starter", "Rare", "Pro", "Epic", "Legendary", "Mythical", "Godly", "Dark", "Void", "Desert"},
-	Callback = function(Value)
-		_G.selectEgg = Value
+	   end,
+	})
+	
+	local Dropdown = Tab:CreateDropdown({
+	   Name = "Select Egg",
+	   Options = {"Noob", "Starter", "Rare", "Pro", "Epic", "Legendary", "Mythical", "Godly", "Dark", "Void", "Desert"},
+	   CurrentOption = "Noob",
+	   Flag = "Dropdown1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	   Callback = function(Option)
+		_G.selectEgg = Option
 		print(_G.selectEgg)
-	end    
-})
-
-local Tab = Window:MakeTab({
-	Name = "Teleport",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
-Tab:AddButton({
-	Name = "Earth",
-	Callback = function()
-			local x = 0.026580367237329483
-			local y = 0.19999952614307404
-			local z = -0.0014024863485246897
-			game.Players.LocalPlayer.Character:MoveTo(Vector3.new(x, y, z))
-  	end    
-})
-
-Tab:AddButton({
-	Name = "Moon",
-	Callback = function()
+	   end,
+	})
+	
+	-- Teleport
+	
+	local Tab = Window:CreateTab("Teleport", 4483362458) -- Title, Image
+	
+	local Button = Tab:CreateButton({
+	   Name = "Earth",
+	   Callback = function()
+		local x = 0.026580367237329483
+		local y = 0.19999952614307404
+		local z = -0.0014024863485246897
+		game.Players.LocalPlayer.Character:MoveTo(Vector3.new(x, y, z))
+	   end,
+	})
+	
+	local Button = Tab:CreateButton({
+		Name = "Moon",
+		Callback = function()
 			local x = 1023.6930541992188
 			local y = 0.19999976456165314
 			local z = 3.8516876697540283
 			game.Players.LocalPlayer.Character:MoveTo(Vector3.new(x, y, z))
-  	end    
-})
-
-Tab:AddButton({
-	Name = "Lava",
-	Callback = function()
+		end,
+	 })
+	
+	 local Button = Tab:CreateButton({
+		Name = "Lava",
+		Callback = function()
 			local x = 2047.70068359375
 			local y = 0.19999976456165314
 			local z = 3.853139638900757
 			game.Players.LocalPlayer.Character:MoveTo(Vector3.new(x, y, z))
-  	end    
-})
-
-local Tab = Window:MakeTab({
-	Name = "Updates",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
-Tab:AddParagraph("v1.0","Switched +5 wins to +12 wins")
-
-Tab:AddParagraph("v1.1","Switched +12 wins to +8 wins")
-
-Tab:AddParagraph("v1.2","Updated the auto farm section")
-
-Tab:AddParagraph("v1.3","Switched +8 wins to +11 (couldn't get the +12 because the drop is glitched")
-
-Tab:AddParagraph("v1.4","Added a auto hatch section")
-
-Tab:AddParagraph("v1.5","Added a teleport section")
-
--- local Tab = Window:MakeTab({
--- 	Name = "Discord",
--- 	Icon = "rbxassetid://4483345998",
--- 	PremiumOnly = false
--- })
-
--- Tab:AddButton({
--- 	Name = "Join discord server",
--- 	Callback = function()
---       		DiscordServerNotification()
-
---               setclipboard("https://discord.gg/BUA8XuPWGX")
---               toclipboard("https://discord.gg/BUA8XuPWGX")
---   	end    
--- })
-
-local Tab = Window:MakeTab({
-	Name = "Gui Options",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
-Tab:AddButton({
-	Name = "Destory GUI",
-	Callback = function()
-        OrionLib:Destroy()
-  	end    
-})
-
-
-
-
-
-
-end
-OrionLib:Init()
+		end,
+	 })
+	
+	-- Updates
+	
+	local Tab = Window:CreateTab("Updates", 4483362458) -- Title, Image
+	
+	local Paragraph = Tab:CreateParagraph({Title = "v1.0", Content = "Switched +5 wins to +12 wins"})
+	
+	local Paragraph = Tab:CreateParagraph({Title = "v1.1", Content = "Switched +12 wins to +8 wins"})
+	
+	local Paragraph = Tab:CreateParagraph({Title = "v1.2", Content = "Updated the auto farm section"})
+	
+	local Paragraph = Tab:CreateParagraph({Title = "v1.3", Content = "Switched +8 wins to +11 (couldn't get the +12 because the drop is glitched)"})
+	
+	local Paragraph = Tab:CreateParagraph({Title = "v1.4", Content = "Added a auto hatch section"})
+	
+	local Paragraph = Tab:CreateParagraph({Title = "v1.5", Content = "Added a teleport section"})
+	
+	local Paragraph = Tab:CreateParagraph({Title = "v1.6", Content = "Switched to a different UI"})
+	
+	-- GUI options
+	
+	local Tab = Window:CreateTab("Gui Options", 4483362458) -- Title, Image
+	
+	local Button = Tab:CreateButton({
+	   Name = "Destory GUI",
+	   Callback = function()
+		Rayfield:Destroy()
+	   end,
+	})
+	
+	
+	
+	
+	end
+	Rayfield:LoadConfiguration()
